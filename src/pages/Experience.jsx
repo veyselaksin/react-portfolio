19db0137cd6abaa12cee3954fa6e97cbbad6c0f4
Veysel AKSIN <veysel.aksin@tga.gov.tr>
2023-06-14 14:54:20 +0300

    Project Structure Designed

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -2,18 +2,50 @@ import React from 'react'
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component'
 import 'react-vertical-timeline-component/style.min.css'
 import { IconSchool } from '@tabler/icons-react'
+import { color } from '@mui/system'
 
 function Experience() {
+    const education = [
+        {
+            id: 1,
+            title: 'Bachelor of Science in Computer Science',
+            date: '2017 - 2021',
+            icon: <IconSchool />,
+            backgroundColor: '#3e497a',
+            color: '#ffffff'
+        },
+        {
+            id: 2,
+            title: 'Bachelor of Science in Philosophy',
+            date: '2017 - 2021',
+            icon: <IconSchool />,
+            backgroundColor: '#e9d35b',
+            color: '#000000'
+        },
+        {
+            id: 3,
+            title: 'Bachelor of Science in Mathematics',
+            date: '2017 - 2021',
+            icon: <IconSchool />,
+            backgroundColor: '#3e497a',
+            color: '#ffffff'
+        }
+    ]
     return (
         <div className='experience'>
             <VerticalTimeline lineColor='#3e497a'>
-                <VerticalTimelineElement
-                    className='vertical-timeline-element--education'
-                    date='2017 - 2021'
-                    iconStyle={{ background: '#3e497a', color: '#ffffff' }}
-                    icon={<IconSchool />}>
-                    <h3 className='vertical-timeline-element-title'>Bachelor of Science in Computer Science</h3>
-                </VerticalTimelineElement>
+                {education.map(item => {
+                    return (
+                        <VerticalTimelineElement
+                            key={item.id}
+                            className='vertical-timeline-element--education'
+                            date={item.date}
+                            iconStyle={{ background: item.backgroundColor, color: item.color }}
+                            icon={item.icon}>
+                            <h3 className='vertical-timeline-element-title'>{item.title}</h3>
+                        </VerticalTimelineElement>
+                    )
+                })}
             </VerticalTimeline>
         </div>
     )
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
+import ProjectItem from '../components/ProjectItem'
+import Project1 from '../assets/project1.png'
+import Project2 from '../assets/project2.png'
+import Project3 from '../assets/project3.jpg'
+import '../styles/Projects.css'
 
 function Projects() {
-    return <div>Projects</div>
+    const projects = [
+        {
+            id: 1,
+            name: 'Project 1',
+            image: Project1
+        },
+        {
+            id: 2,
+            name: 'Project 2',
+            image: Project2
+        },
+        {
+            id: 3,
+            name: 'Project 3',
+            image: Project3
+        }
+    ]
+    return (
+        <div className='projects'>
+            <h1>Projects</h1>
+            <div className='projectList'>
+                {projects.map(item => {
+                    return <ProjectItem name={item.name} image={item.image} />
+                })}
+            </div>
+        </div>
+    )
 }
 
 export default Projects
